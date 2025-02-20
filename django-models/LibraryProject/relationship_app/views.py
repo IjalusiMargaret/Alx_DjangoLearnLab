131944df1c2e508ae2bf3dcdfaab99dcd5bc54f1
Ijalusi Margaret <ijalusimargaret@gmail.com>
2025-02-20 13:22:01 +0100

    task 3 done

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -9,6 +9,35 @@ from django.contrib.auth import logout
 from django.contrib.auth import authenticate
 from django.contrib.auth.forms import AuthenticationForm 
 from django.contrib.auth.forms import UserCreationForm
+#from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.models import User
+
+# Helper functions for role checking
+def is_admin(user):
+    return user.is_authenticated and user.userprofile.role == "Admin"
+
+def is_librarian(user):
+    return user.is_authenticated and user.userprofile.role == "Librarian"
+
+def is_member(user):
+    return user.is_authenticated and user.userprofile.role == "Member"
+
+# Admin View
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_view.html', {'user': request.user})
+
+# Librarian View
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'librarian_view.html', {'user': request.user})
+
+# Member View
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member_view.html', {'user': request.user})
+
 
 def user_login(request):
     if request.method == "POST":
