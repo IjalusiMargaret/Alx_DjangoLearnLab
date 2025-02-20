5ec859a42d57e5512cea459ed76ce67e7c32b450
Ijalusi Margaret <ijalusimargaret@gmail.com>
2025-02-20 14:09:02 +0100

    task 4 done

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,17 +2,8 @@ from django.urls import path
 from .views import list_books, LibraryDetailView
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
-#from django.urls import path
 from .views import admin_view, librarian_view, member_view
 from .views import add_book, edit_book, delete_book
-
-urlpatterns = [
-    path('add-book/', add_book, name='add_book'),
-    path('edit-book/<int:book_id>/', edit_book, name='edit_book'),
-    path('delete-book/<int:book_id>/', delete_book, name='delete_book'),
-]
-
-
 
 urlpatterns = [
     path('add-book/', add_book, name='add_book'),
