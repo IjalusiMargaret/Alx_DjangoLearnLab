14b60ec7805e67768ba06a0805740f3978c0b66d
Ijalusi Margaret <ijalusimargaret@gmail.com>
2025-03-09 16:53:20 +0100

    Added Book API endpoint

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import BookList
+from .views import BookList  # Ensure you import BookList
 
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),
+    path('books/', BookList.as_view(), name='book-list'),  # Using Django's path() function
 ]
