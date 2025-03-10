0c02cd556fe9dd4f11194949e7883848b4941fed
Ijalusi Margaret <ijalusimargaret@gmail.com>
2025-03-10 16:34:48 +0100

    task 0 done

---
+++ advanced_api_project/api/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+#from django.db import models
+from django.core.exceptions import ValidationError
+from datetime import datetime
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+"""
+    Represents an author with a name.
+    The 'name' field stores the author's full name.
+    """
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey('Author', related_name='books', on_delete=models.CASCADE)
+
+    def clean(self):
+        current_year = datetime.now().year
+        if self.publication_year > current_year:
+            raise ValidationError({"publication_year": "Publication year cannot be in the future."})
+
+    def save(self, *args, **kwargs):
+        self.clean()  # Call clean() before saving
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
