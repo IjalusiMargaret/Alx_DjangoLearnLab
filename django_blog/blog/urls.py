35fe78cb028ea687e73629208468a00a381f4a7f
Ijalusi Margaret <ijalusimargaret@gmail.com>
2025-03-20 09:03:54 +0100

    task 2 done

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,6 +1,5 @@
-#from django.urls import path
-from . import views
 from django.urls import path
+from . import views
 from .views import (
     PostListView,
     PostDetailView,
@@ -9,20 +8,17 @@ from .views import (
     PostDeleteView,
 )
 
-
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.register_view, name='register'),
     path('profile/', views.profile_view, name='profile'),
     path('', views.home, name='home'),  # Homepage
-    path('posts/', views.post_list, name='posts'),
-    path('', PostListView.as_view(), name='post-list'),
+    
+    # CRUD URLs (using class-based views)
+    path('post/', PostListView.as_view(), name='post-list'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-    # blog/urls.py
-
-
 ]
