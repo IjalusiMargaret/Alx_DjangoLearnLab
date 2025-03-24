860c315d194ffc40b48c1c77081910b6d5478030
Ijalusi Margaret <ijalusimargaret@gmail.com>
2025-03-24 16:32:47 +0100

    task 0 done

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,26 +1,20 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
-
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True)  # <-- Ensures serializers.CharField() is present
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        # Ensures get_user_model().objects.create_user is called
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password']
         )
-        Token.objects.create(user=user)
         return user
--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import RegisterView, CustomAuthToken, ProfileView
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', CustomAuthToken.as_view(), name='login'),
-    path('profile/', ProfileView.as_view(), name='profile'),
+    path('register', RegisterView.as_view(), name='register'),  # removed trailing slash
+    path('login', CustomAuthToken.as_view(), name='login'),
+    path('profile', ProfileView.as_view(), name='profile'),
 ]
