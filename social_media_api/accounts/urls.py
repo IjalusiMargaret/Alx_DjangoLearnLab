4c310d120d591f8fe4d566cd59171d3def2d18dd
Ijalusi Margaret <ijalusimargaret@gmail.com>
2025-03-27 12:33:29 +0100

    fix

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,26 +1,9 @@
 from django.urls import path
-from .views import RegisterView, CustomAuthToken, ProfileView
+from rest_framework.authtoken.views import ObtainAuthToken
+from .views import RegisterView, ProfileView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', CustomAuthToken.as_view(), name='login'),
+    path('login/', ObtainAuthToken.as_view(), name='login'),
     path('profile/', ProfileView.as_view(), name='profile'),
 ]
-
-
-
-
-
-
-
-
-
-'''from django.urls import path
-from .views import RegisterView, CustomAuthToken, ProfileView
-
-urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),  # Ensure /register exists
-    path('login/', CustomAuthToken.as_view(), name='login'),  # Ensure /login exists
-    path('profile/', ProfileView.as_view(), name='profile'),  # Ensure /profile exists
-]
-'''
